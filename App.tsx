cc24297d094bbc1d33162a129de549e4fb4a01ef
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-24 13:02:01 +0000

    set up inter font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens/Home';
 import { TasksContextProvider } from './src/context/tasks';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'; 
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <TasksContextProvider>
       <Home />
