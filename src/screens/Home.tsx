792b711a0e5363f0476ec9b4eff720557132e842
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-26 17:21:46 +0000

    style tasks list container

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,124 +14,124 @@ export function Home() {
       />
 
       {/* Input */}
-      <NewTaskInput 
-        onAddTask={addTask}
-      />
-
-      {/* Tasks Summary */}
-      <View style={styles.tasksSummaryContainer}>
-        {/* Tasks Summary Item - Created */}
-        <View style={styles.tasksSummaryItemContainer}>
-          <Text style={styles.tasksCreatedLabel}>
-            Created:
-          </Text>
-          <View style={styles.summaryBadge}>
-            <Text
-              style={styles.summaryBadgeText}
-            >
-              {tasksSummary().total}
-            </Text>
-          </View>
-        </View>
-
-        {/* Tasks Summary Item - Done */}
-        <View style={styles.tasksSummaryItemContainer}>
-          <Text style={styles.tasksDoneLabel}>
-            Done:
-          </Text>
-          <View style={styles.summaryBadge}>
-            <Text
-              style={styles.summaryBadgeText}
-            >
-              {tasksSummary().done}
-            </Text>
-          </View>
-        </View>
+      <View style={styles.inputContainer}>
+        <NewTaskInput 
+          onAddTask={addTask}
+        />
       </View>
 
-      <FlatList 
-        data={tasks}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) => (
-          // Visual component to display and change status 
-          <View style={{
-            flexDirection: 'row',
-            marginBottom: 20,
-            alignItems: 'center',
-            gap: 20,
-          }}>
-            {/* Checkbox and Title */}
-            <TouchableOpacity
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                flex: 1,
-                gap: 10,
-                borderWidth: 1,
-                borderColor: '#000',
-                height: 40,
-              }}
-              onPress={() => {
-                changeTaskStatus(item.id, item.status === TasksStatus.PENDING ? TasksStatus.DONE : TasksStatus.PENDING)
-              }}
-            >
-              <View
-              style={{
-                height: 20,
-                width: 20,
-                borderRadius: 12,
-                borderWidth: 1,
-                borderColor: '#fff',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
+      <View style={styles.bodyContainer}>
+        {/* Tasks Summary */}
+        <View style={styles.tasksSummaryContainer}>
+          {/* Tasks Summary Item - Created */}
+          <View style={styles.tasksSummaryItemContainer}>
+            <Text style={styles.tasksCreatedLabel}>
+              Created:
+            </Text>
+            <View style={styles.summaryBadge}>
+              <Text
+                style={styles.summaryBadgeText}
               >
-                {
-                  item.status === 'done' &&
-                  <View style={{
-                    height: 12,
-                    width: 12,
-                    borderRadius: 6,
-                    backgroundColor: 'green',
-                  }} />
-                }
-              </View>
-
-              <Text 
-                style={{
-                  fontSize: 16, 
-                  lineHeight: 16,
-                  flex: 1,
-                  color: item.status === 'done' ? '#808080' : '#fff',
-                  textDecorationLine: item.status === 'done' ? 'line-through' : 'none',
-              }}>
-                {item.title}
+                {tasksSummary().total}
               </Text>
-            </TouchableOpacity>
-            
-            {/* Remove button */}
-            <TouchableOpacity onPress={() => removeTask(item.id)}>
-              <View style={{
-                height: 40,
-                width: 40,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'red',
-                borderRadius: 4,
-              }}>
+            </View>
+          </View>
+
+          {/* Tasks Summary Item - Done */}
+          <View style={styles.tasksSummaryItemContainer}>
+            <Text style={styles.tasksDoneLabel}>
+              Done:
+            </Text>
+            <View style={styles.summaryBadge}>
+              <Text
+                style={styles.summaryBadgeText}
+              >
+                {tasksSummary().done}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <FlatList 
+          data={tasks}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            // Visual component to display and change status 
+            <View style={styles.tasksListContainer}>
+              {/* Checkbox and Title */}
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  flex: 1,
+                  gap: 10,
+                  borderWidth: 1,
+                  borderColor: '#000',
+                  height: 40,
+                }}
+                onPress={() => {
+                  changeTaskStatus(item.id, item.status === TasksStatus.PENDING ? TasksStatus.DONE : TasksStatus.PENDING)
+                }}
+              >
+                <View
+                style={{
+                  height: 20,
+                  width: 20,
+                  borderRadius: 12,
+                  borderWidth: 1,
+                  borderColor: '#fff',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                >
+                  {
+                    item.status === 'done' &&
+                    <View style={{
+                      height: 12,
+                      width: 12,
+                      borderRadius: 6,
+                      backgroundColor: 'green',
+                    }} />
+                  }
+                </View>
+
                 <Text 
                   style={{
-                    color: '#fff', 
-                    fontSize: 20, 
-                    lineHeight: 20
+                    fontSize: 16, 
+                    lineHeight: 16,
+                    flex: 1,
+                    color: item.status === 'done' ? '#808080' : '#fff',
+                    textDecorationLine: item.status === 'done' ? 'line-through' : 'none',
                 }}>
-                  -
+                  {item.title}
                 </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
+              </TouchableOpacity>
+              
+              {/* Remove button */}
+              <TouchableOpacity onPress={() => removeTask(item.id)}>
+                <View style={{
+                  height: 40,
+                  width: 40,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: 'red',
+                  borderRadius: 4,
+                }}>
+                  <Text 
+                    style={{
+                      color: '#fff', 
+                      fontSize: 20, 
+                      lineHeight: 20
+                  }}>
+                    -
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
+
   )
 }
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -5,17 +5,30 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#0D0D0D',
     paddingTop: 30,
-    paddingHorizontal: 20,
   },
   logo: {
     alignSelf: 'center',
     marginBottom: 20,
     marginTop: 50,
   },
+  inputContainer:{
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  bodyContainer: {
+    flex: 1,
+    zIndex: -1,
+    backgroundColor: '#1A1A1A',
+    paddingHorizontal: 20,
+    paddingTop: 60,
+    paddingBottom: 20,
+    marginTop: -80,
+  },
   tasksSummaryContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 20,
+    paddingHorizontal: 20,
   },
   tasksSummaryItemContainer:{
     flexDirection: 'row',
@@ -48,5 +61,11 @@ export const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "700",
     color: "#D9D9D9"
+  },
+  tasksListContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    alignItems: 'center',
+    gap: 20,
   },
 });
